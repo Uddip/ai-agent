bee25571d7de3b17ded6f864e7fbd7c6bbab9160
Uddip Amin <uddip.amin@gmail.com>
2025-11-22 21:58:53 -0500

    Feature - User input as argv to pass to genai as tokens

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
+MODEL_NAME = "gemini-2.0-flash-001"
+
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py '<input text>'")
+        sys.exit(1)
+        
     load_dotenv()
     api_key = os.getenv('GEMINI_API_KEY')
 
     client = genai.Client(api_key=api_key)
+    input = sys.argv[1]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model=MODEL_NAME,
+        contents=input
     )
 
     print(response.text)
