c4c050b9e104a7d4f9945688e19eec9d2fd35527
Uddip Amin <uddip.amin@gmail.com>
2025-11-22 22:54:01 -0500

    Feature - Prompts and responses contained in a message list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 MODEL_NAME = "gemini-2.0-flash-001"
 
@@ -9,21 +10,27 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py '<input text>'")
         sys.exit(1)
-        
+
     load_dotenv()
     api_key = os.getenv('GEMINI_API_KEY')
 
     client = genai.Client(api_key=api_key)
-    input = sys.argv[1]
+    user_input = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_input)])
+    ]
 
     response = client.models.generate_content(
         model=MODEL_NAME,
-        contents=input
+        contents=messages,
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_input}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
