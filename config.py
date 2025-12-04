6426ec267b0896132089a1e20f86a5c973190deb
Uddip Amin <uddip.amin@gmail.com>
2025-12-03 22:45:11 -0500

    Updated config.py and completed get_file_content functionality

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 MAX_CHARS = 10000
+TRUNC_MESSAGE = "[...File \"{file_path}\" truncated at 10000 characters]"
--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -12,7 +12,13 @@ def get_file_info(working_directory, file_path):
             raise Exception(f"File not found or if not a regular file: \"{file_path}\" ")
 
         with open(file_path, "r") as file:
-            file_content_string = file.read(MAX_CHARS)
+            file_content_string = file.read()
+
+        if len(file_content_string) > MAX_CHARS:
+            file_content_string = file_content_string[:10001] + f"[...File \"{file_path}\" truncated at 10000 characters]"
+
+        print(file_content_string)
 
     except Exception as e:
         return str(f"Error: {e}")
+
