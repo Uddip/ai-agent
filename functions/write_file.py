0bcf81ca736f7f47b2cbc9edd3effa71ef6cdf63
Uddip Amin <uddip.amin@gmail.com>
2025-12-07 00:20:24 -0500

    Passing write_file tests

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,17 +3,18 @@ import os
 def write_file(working_directory, file_path, content):
     try:
         absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+        working_directory_path = os.path.abspath(working_directory)
 
-        if not absolute_path.startswith(os.path.abspath(working_directory)):
+        if not absolute_path.startswith(working_directory_path):
             raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
 
         if not os.path.exists(absolute_path):
-            os.makedirs(os.path.dirname(absolute_path))
+            os.makedirs(os.path.dirname(absolute_path), exist_ok=True)
 
-        with open(file_path, "w") as f:
+        with open(absolute_path, "w") as f:
             f.write(content)
 
-        print(f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
         return str(f"Error: {e}")
--- test_write_file.py
+++ test_write_file.py
@@ -1,17 +1,40 @@
 import unittest
 from functions.write_file import write_file
 
-def TestWriteFile(unittest.TestCase):
+class TestWriteFile(unittest.TestCase):
     def setUp(self):
         self.working_directory = "calculator"
 
     def test_write_existing_file(self):
+        file_path = "lorem.txt"
+        content = "wait, this isn't lorem ipsum"
+        expected = "28 characters written"
 
+        result = write_file(self.working_directory, file_path, content)
+
+        self.assertIn(expected, result)
+        print(result)
 
     def test_write_new_file(self):
+        file_path = "pkg/morelorem.txt"
+        content = "lorem ipsum dolor sit amet"
+        expected = "26 characters written"
 
+        result = write_file(self.working_directory, file_path, content)
+
+        self.assertIn(expected, result)
+        print(result)
 
     def test_write_error_outside_working_directory(self):
+        file_path = "/tmp/temp.txt"
+        content = "this should not be allowed"
+        expected = "Error:"
+
+        result = write_file(self.working_directory, file_path, content)
+
+        self.assertIn(expected, result)
+        print(result)
+
 
 if __name__ == "__main__":
     unittest.main()
