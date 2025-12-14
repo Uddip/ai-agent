0dcd3ce028ace97b6797e2a906b9f6767f3972ce
Uddip Amin <uddip.amin@gmail.com>
2025-12-14 00:25:53 -0500

    created run_python_file functionality

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,32 @@
+import os
 
 def run_python_file(working_directory, file_path, args=[]):
+    try:
+        abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+        abs_working_dir = os.path.abspath(working_directory)
+
+        if not abs_file_path.startswith(abs_working_dir):
+            raise Exception(f'Cannot write to "{file_path}" as it is outside the permitted working directory')
+
+        if not os.path.exists(abs_file_path):
+            raise Exception(f'File "{file_path}" not found.')
+
+        if not abs_file_path.endswith('.py'):
+            raise Exception(f'"{file_path}" is not a Python file.')
+
+        completed_process = subprocess.run(['python', working_dir, file_path, args], timeout=30, capture_output=True)
+
+        if not completed_process:
+            return f'No output produced'
+
+        output = f'STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}'
+
+        if not completed_process.returncode == 0:
+            output += f'\nProcess exited with code {completed_process.returncode}'
+
+        return output
+
+    except Exception as e:
+        return str(f"Error: executing Python file: {e}")
 
 
