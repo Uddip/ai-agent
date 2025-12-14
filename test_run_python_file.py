65dba8ba95fb3e29963d465bfe79b7c3ffdb9062
Uddip Amin <uddip.amin@gmail.com>
2025-12-14 00:36:39 -0500

    added test_run_python_file

---
+++ test_run_python_file.py
@@ -0,0 +1,19 @@
+import unittest
+from functions.run_python_file import run_python_file
+
+class TestRunPythonFile(unittest.TestCase):
+    def setUp(self):
+        self.working_directory = "calculator"
+
+    def test_print_usage_instructions_for_calculator(self):
+
+    def test_run_calculator(self):
+
+    def test_run_calculator_tests(self):
+
+    def test_run_python_file_to_error_outside_working_dir(self):
+
+    def test_run_python_file_to_error_nonexistent_file(self):
+
+    def test_run_python_file_to_error_not_py_file(self):
+
