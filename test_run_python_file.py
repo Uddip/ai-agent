af4515e3d0e248182093377fa7941ce57ae8d657
Uddip Amin <uddip.amin@gmail.com>
2025-12-18 18:38:02 -0500

    Finished adding tests

--- test_run_python_file.py
+++ test_run_python_file.py
@@ -25,12 +25,41 @@ class TestRunPythonFile(unittest.TestCase):
         print(result)
 
     def test_run_calculator_tests(self):
+        file_path = "tests.py"
+        expected = "Ran 9 tests"
+
+        result = run_python_file(self.working_directory, file_path)
+
+        self.assertIn(expected, result)
+        print(result)
 
     def test_run_python_file_to_error_outside_working_dir(self):
+        file_path = "../main.py"
+        expected = 'Cannot execute "../main.py" as it is outside'
+
+        result = run_python_file(self.working_directory, file_path)
+
+        self.assertIn(expected, result)
+        print(result)
 
     def test_run_python_file_to_error_nonexistent_file(self):
+        file_path = "nonexistent.py"
+        expected = '"nonexistent.py" does not exist'
+
+        result = run_python_file(self.working_directory, file_path)
+
+        self.assertIn(expected, result)
+        print(result)
 
     def test_run_python_file_to_error_not_py_file(self):
+        file_path = "lorem.txt"
+        expected = '"lorem.txt" is not a Python file'
+
+        result = run_python_file(self.working_directory, file_path)
+
+        self.assertIn(expected, result)
+        print(result)
+
 
 if __name__ == "__main__":
     unittest.main()
