9359a0803b42788e1450d4acdd0840325ecd9a6c
Uddip Amin <uddip.amin@gmail.com>
2025-12-20 15:29:12 -0500

    All run_python_file tests passing

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -7,25 +7,23 @@ def run_python_file(working_directory, file_path, args=[]):
         abs_working_dir = os.path.abspath(working_directory)
 
         if not abs_file_path.startswith(abs_working_dir):
-            raise Exception(f'Cannot write to "{file_path}" as it is outside the permitted working directory')
+            raise Exception(f'Cannot execute "{file_path}" as it is outside the permitted working directory')
 
         if not os.path.exists(abs_file_path):
-            raise Exception(f'File "{file_path}" not found.')
+            raise Exception(f'File "{file_path}" does not exist or is not a regular file')
 
         if not abs_file_path.endswith('.py'):
             raise Exception(f'"{file_path}" is not a Python file.')
-        
-        file_args = [working_directory, file_path, *args]
 
-        completed_process = subprocess.run(['python', abs_file_path, *args], timeout=30, capture_output=True)
+        completed_process = subprocess.run(['python', abs_file_path, *args], timeout=30, capture_output=True, text=True)
 
         if not completed_process:
             return f'No output produced'
 
-        output = f'STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}'
+        output = f"STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}"
 
         if not completed_process.returncode == 0:
-            output += f'\nProcess exited with code {completed_process.returncode}'
+            output += f"\nProcess exited with code {completed_process.returncode}"
 
         return output
 
