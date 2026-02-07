12962c5c6cbf25efc1e807d1fd5075bea775193a
Uddip Amin <uddip.amin@gmail.com>
2026-02-07 13:44:21 -0500

    Added function calling logic for agent

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 MAX_CHARS = 10000
+WORKING_DIR = "./calculator"
 
 def trunc_message(file_path):
     return f"[...File \"{file_path}\" truncated at 10000 characters]"
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 MODEL_NAME = "gemini-2.0-flash-001"
 
@@ -61,8 +61,22 @@ def generate_content(client, messages, verbose):
         print(response.text)
         return
     
+    function_results = []
+    
     for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        result = call_function(function_call, verbose)
+        if (
+            not result.parts
+            or not result.parts[0].function_response
+            or not result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty function response for {function_call.name}")
+        if verbose:
+            print(f"-> {result.parts[0].function_response.response}")
+            
+        function_results.append(result.parts[0])
+
+    
 
 if __name__ == "__main__":
     main()
